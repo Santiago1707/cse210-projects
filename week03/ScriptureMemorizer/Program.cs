5b2e34cbe5529f7a86efd969689f5720a25d1b07
Santiago1707 <dmendozaladino@byupathway.edu>
2025-03-21 17:15:36 -0400

    .

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -10,7 +10,7 @@ class Program
         {
             new Scripture(new Reference("John", 3, 16), "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."),
             new Scripture(new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths."),
-            new Scripture(new Reference("Philippians", 4, 13), "I can do all things through Christ who strengthens me")
+            new Scripture(new Reference("Philippians", 4, 13), "I can do all things through Christ who strengthens me.")
         };
 
         Console.WriteLine("Select a scripture to memorize:");
@@ -49,7 +49,7 @@ class Program
             {
                 Console.Clear();
                 Console.WriteLine(selectedScripture.GetDisplayText());
-                Console.WriteLine("\nAll words are hidden! Program will now exit.");
+                Console.WriteLine("\nAll words are hidden!");
                 break;
             }
         }
