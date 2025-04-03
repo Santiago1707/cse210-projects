4e52c0e9d48e5b36c566306ba62131362ddbe52b
Santiago1707 <dmendozaladino@byupathway.edu>
2025-04-03 13:16:38 -0400

    homework task

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Alice Brown", "Algebra", "7.3", "3-10, 20-21");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
